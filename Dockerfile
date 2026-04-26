7afe515241efb9f5ccc4502461c9ccc1a5e4ccae
ish1416 <ish1442006@gmail.com>
2026-04-26 10:34:51 +0530

    docker-changed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# Stage 1: Build frontend
+
 FROM node:20-alpine AS frontend-build
 WORKDIR /app/frontend
 COPY frontend/package*.json ./
@@ -6,7 +6,7 @@ RUN npm install
 COPY frontend/ ./
 RUN npm run build
 
-# Stage 2: Run backend + serve frontend
+
 FROM node:20-alpine
 WORKDIR /app/backend
 COPY backend/package*.json ./
--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,7 @@ const path = require("path");
 const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-});
+
 
 
 
@@ -22,6 +19,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
